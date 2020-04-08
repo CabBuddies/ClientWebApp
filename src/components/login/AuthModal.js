66fa9be76970679ca83590dfac7edffb5f74f045
ArunKarthik2197 <arun.munipalle1997@gmail.com>
2020-04-07 23:18:05 -0700

    Update AuthModal.js

--- src/components/login/AuthModal.js
+++ src/components/login/AuthModal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal,Button } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 import SocialLogin from './SocialLogin'
 import AuthTabs from './AuthTabs'
 import { Icon, Header } from 'semantic-ui-react'
